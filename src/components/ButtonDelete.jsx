5e25132e87864d0cb77b299cc172946f615a889d
arklem <jrsy.tdh@yandex.ru>
2024-01-22 02:04:15 +0300

    update cart

---
+++ src/components/ButtonDelete.jsx
@@ -0,0 +1,52 @@
+import '../styles/ButtonDelete.css';
+import React, { FC, useState, useEffect } from 'react';
+import { useDispatch } from 'react-redux';
+import { GetData } from '../getData';
+import { useData, deleteAppointment, sendAppointment } from '../slices/dataSlice';
+
+
+const ButtonDelete = ({ appointment }) => {
+
+    const dispatch = useDispatch();
+
+    const [isActiveAdd, setIsActiveAdd] = useState(true);
+    
+    GetData();
+
+    const data = useData();
+
+    console.log(data);
+
+    useEffect(() => {
+        const appointmentIds = data.map(item => item?.id);
+        setIsActiveAdd(!appointmentIds.includes(appointment.id));
+    }, [data, appointment.id]);
+
+    const handleAddClick = () => {
+
+        setIsActiveAdd(false);
+
+        dispatch(sendAppointment(appointment.id))
+
+    };
+
+    const handleDeleteClick = () => {
+
+        dispatch(deleteAppointment(appointment.id))
+
+        setIsActiveAdd(true);
+
+    };
+
+    return (
+        <div className="cardButton">
+            {!isActiveAdd ? (
+                <button className='delete' onClick={handleDeleteClick}>Удалить</button>
+            ) : (
+                <button className='add' onClick={handleAddClick}>Добавить</button>
+            )}
+        </div>
+    );
+};
+
+export default ButtonDelete;
