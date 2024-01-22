0c9e73cb479b4a87dc825a6c911d5757d9736114
arklem <jrsy.tdh@yandex.ru>
2024-01-22 21:43:55 +0300

    update buttondeleteadd

--- src/components/ButtonDeleteAdd.jsx
+++ src/components/ButtonDeleteAdd.jsx
@@ -1,13 +1,26 @@
 import '../styles/ButtonDeleteAdd.css';
 import React, { FC, useState, useEffect } from 'react';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
+import { Link } from 'react-router-dom';
 import { GetData } from '../getData';
+import { updateUser } from '../slices/auth';
 import { useData, deleteAppointment, sendAppointment } from '../slices/dataSlice';
 
 
 const ButtonDeleteAdd = ({ appointment }) => {
 
     const dispatch = useDispatch();
+
+    const { user: currentUser } = useSelector((state) => state.auth);
+
+    useEffect(() => {
+        const storedUser = JSON.parse(localStorage.getItem("user"));
+
+        if (storedUser) {
+            dispatch(updateUser(storedUser)); 
+        }
+
+    }, [dispatch]);
 
     const [isActiveAdd, setIsActiveAdd] = useState(true);
     
@@ -40,9 +53,13 @@ const ButtonDeleteAdd = ({ appointment }) => {
         <div className="cardButton">
             {!isActiveAdd ? (
                 <button className='delete' onClick={handleDeleteClick}>Удалить</button>
-            ) : (
+            ) : (currentUser ? (
                 <button className='add' onClick={handleAddClick}>Добавить</button>
-            )}
+            ) : (
+                <Link to='/login' className='add'>
+                    Добавить
+                </Link>
+            ))}
         </div>
     );
 };
