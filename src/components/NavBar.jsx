9eab08239eb25f626e21c61096c683ecfc354b6f
arklem <jrsy.tdh@yandex.ru>
2024-01-26 00:44:33 +0300

    update

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -41,35 +41,48 @@ const NavBar = () => {
                     Главная
                 </Link>
 
-            {currentUser ? (
-                <div className="navbar-nav ml-auto">
-                <li className="nav-item">
-                    <Link to={"/"} className="nav-link">
-                        { currentUser.username }
-                    </Link>
-                </li>
-                <li className="nav-item">
-                    <div className="cart-icon">
-                        <Link to={"/shopcart"} className="nav-link">
-                            <FaShoppingCart className="cart" />
-                            {data.length > 0 && <span className="cart-badge">{data.length}</span>}
+                {currentUser ? (
+                    <div className="navbar-nav ml-auto">
+                    <li className="nav-item">
+                        <Link to={"/"} className="nav-link">
+                            { currentUser.username }
                         </Link>
+                    </li>
+                        {moderator && (
+                            <>
+                                <li className="nav-item">
+                                    <Link to={"/applications"} className="nav-link">
+                                        Заявки
+                                    </Link> 
+                                    </li>
+                                    <li className="nav-item">
+                                    <Link to={"/appointments"} className="nav-link">
+                                        Услуги
+                                    </Link>
+                                </li>
+                            </>
+                        )} 
+                        <li className="nav-item">
+                        <div className="cart-icon">
+                            <Link to={"/shopcart"} className="nav-link">
+                                <FaShoppingCart className="cart" />
+                                {data.length > 0 && <span className="cart-badge">{data.length}</span>}
+                            </Link>
+                        </div>
+                        </li>
+                    <li className="nav-item">
+                        <Link to={"/login"} className="nav-link" onClick={logOut}>
+                            Войти
+                        </Link>
+                    </li>
                     </div>
-
-                </li>
-                <li className="nav-item">
-                    <a href="/login" className="nav-link" onClick={logOut}>
-                        Выйти
-                    </a>
-                </li>
-                </div>
-            ) : (
-                <div className="navbar-nav ml-auto">
-                <li className="nav-item">
-                    <Link to={"/login"} className="nav-link">
-                        Войти
-                    </Link>
-                </li>
+                ) : (
+                    <div className="navbar-nav ml-auto">
+                    <li className="nav-item">
+                        <Link to={"/login"} className="nav-link">
+                            Войти
+                        </Link>
+                    </li>
 
                     <li className="nav-item">
                         <Link to={"/register"} className="nav-link">
