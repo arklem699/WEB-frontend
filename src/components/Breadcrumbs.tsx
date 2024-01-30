d65164d7c3de301138c7f72be1e93cc4d637e83a
arklem <jrsy.tdh@yandex.ru>
2024-01-30 22:11:26 +0300

    update Breadcrumbs.tsx

--- src/components/Breadcrumbs.tsx
+++ src/components/Breadcrumbs.tsx
@@ -14,7 +14,7 @@ const Breadcrumbs: FC = () => {
     const topics: { [key: string]: string } = {
         "post": "Создать",
         "update": "Редактировать",
-        "appointments": "Услуги",
+        "appointments": "Доступное время",
         "application": "Черновик",
         "my": "Мои заявки",
         "login": "Вход",
@@ -35,7 +35,7 @@ const Breadcrumbs: FC = () => {
                 <div className={"crumb"} key={crumb}>
                     <FaChevronRight className={"chevron-icon"}/>
                     <Link to={currentLink}>
-                        Услуга №{appointmentNumber}
+                        Доступное время №{appointmentNumber}
                     </Link>
                 </div>
             )
